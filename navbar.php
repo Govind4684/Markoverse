bd568ff1a9a9221cc451c30710a75955739c867f
Govind <kumargovindkarn@gmail.com>
2020-12-08 00:16:25 +0530

    navbar header class

--- navbar.php
+++ navbar.php
@@ -14,7 +14,7 @@
   </head>
   <body>
    
-    <header class="header-transparent" id="header-main">
+    <header class="header-transparent text-light" id="header-main">
         
         <div class="navbar-top navbar-light bg-light">
             <div class="container">
